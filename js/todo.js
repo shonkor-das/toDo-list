e57907de5e87bb5f04639e584585912695aef790
Shonkor Das <94380595+shonkor-das@users.noreply.github.com>
2022-08-18 11:21:58 +0600

    all to do code added

--- js/todo.js
+++ js/todo.js
@@ -1,5 +1,22 @@
 
+let count = 0;
+
 document.getElementById('input-btn').addEventListener('click', function(){
+    count++;
     const inputValue = document.getElementById('input-value').value;
-    
-})
+    document.getElementById('input-value').value = '';
+
+    const mainContainer = document.getElementById('content-container');
+    const tableContainer = document.createElement("tr");
+    // useing template string 
+    tableContainer.innerHTML=`
+    <th scope="row">${count}</th>
+    <td>${inputValue}</td>
+    <td>
+    <button class="btn btn-primary">Delete</button>
+    <button class="btn btn-warning">Edit</button>
+    </td>
+    `;
+   
+    mainContainer.appendChild(tableContainer);
+});
